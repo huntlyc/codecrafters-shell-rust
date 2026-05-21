5fc1034465f459c05bd57ec4df1778b1e7464bfd
Huntly Cameron <huntly.cameron@gmail.com>
2026-05-21 21:51:10 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 #[allow(unused_imports)]
 use std::io::{self, Read, Write};
 
+struct Command {
+    name: String,
+    args: Vec<String>,
+}
+
 fn main() {
     loop {
         print_prompt();
-        let cmd = read_input();
+        let input = read_input();
+        let cmd = parse_command_from_input(input);
 
-        if !is_valid_command(&cmd) {
-            println!("{}: command not found", cmd);
+        if !is_valid_command(&cmd.name) {
+            println!("{}: command not found", cmd.name);
+            continue;
         }
 
         run_cmd(cmd)
@@ -34,14 +41,33 @@ fn read_input() -> String {
     return input;
 }
 
+fn parse_command_from_input(input: String) -> Command {
+    let mut parts = input.split_whitespace();
+    let cmd_name = parts.next().unwrap().to_string();
+    let mut cmd_args: Vec<String> = Vec::new();
+
+    for (_, a) in parts.enumerate() {
+        cmd_args.push(a.to_string())
+    }
+
+    Command {
+        name: cmd_name,
+        args: cmd_args,
+    }
+}
+
 fn is_valid_command(cmd: &String) -> bool {
-    let cmds = ["exit"];
+    let cmds = ["echo", "exit"];
 
     return cmds.iter().any(|c| c == cmd);
 }
 
-fn run_cmd(cmd: String) {
-    if cmd.to_lowercase() == "exit" {
-        std::process::exit(0)
+fn run_cmd(cmd: Command) {
+    match cmd.name.as_str() {
+        "exit" => std::process::exit(0),
+        "echo" => {
+            println!("{}", cmd.args.join(" "))
+        }
+        _ => std::process::exit(1),
     }
 }
