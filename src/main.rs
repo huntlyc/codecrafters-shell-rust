35b31e8a17155a725bdb48247448a24fb9bfe620
Huntly Cameron <huntly.cameron@gmail.com>
2026-05-21 20:57:46 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 
 fn main() {
+    print_prompt();
+    read_input();
+}
+
+fn print_prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("should have provided input");
+
+    input = input.trim().to_string();
+    if input == "" {
+        print_prompt();
+        input = read_input()
+    }
+
+    println!("{}: command not found", input);
+
+    return input;
 }
